56808d5f00e5e38690a73e5f2598fa4d53fab24e
Adam Pruner <adampruner99@gmail.com>
2019-10-31 14:08:55 -0700

    set up file reading so it's easy for the students

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
+import java.io.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String s1Result = seniorProblem1("2016CCCTestData/s1/s1.1.in");
+        System.out.println(s1Result);
+    }
+
+    // 2016 CCC Senior problem 1
+    public static String seniorProblem1(String testDataFilePath) {
+        String first;
+        String second;
+        File file = new File(testDataFilePath);
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            first = br.readLine();
+            second = br.readLine();
+            System.out.println(String.format("%s, %s", first, second));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "N";
     }
 }
